25b9e9d6362f5cc094e23e04633a9e3603ff225c
Jose Medina <jose.medina@epfl.ch>
2017-04-23 18:55:21 +0200

    corrects transforms

--- cpr_bringup/src/fake_wrench.py
+++ cpr_bringup/src/fake_wrench.py
@@ -3,6 +3,7 @@
 import rospy
 import copy
 import tf
+import numpy
 
 from interactive_markers.interactive_marker_server import *
 from interactive_markers.menu_handler import *
@@ -15,27 +16,43 @@ marker_pose = geometry_msgs.msg.Transform()
 listener = None
 wrench_pub = None
 
-def frameCallback( msg ):
+def publisherCallback( msg ):
     try:
-        listener.waitForTransform("/world", "/ur5_arm_ee_link", rospy.Time(0), rospy.Duration(7.0))
-        (trans,rot1) = listener.lookupTransform("/world", "/ur5_arm_ee_link", rospy.Time(0))
+        listener.waitForTransform("/fake_force_pose", "/world", rospy.Time(0), rospy.Duration(10.0))
+        (trans1,rot1) = listener.lookupTransform("/world", "/fake_force_pose", rospy.Time(0))
+        (trans2,rot2) = listener.lookupTransform("/world", "/ur5_arm_ee_link", rospy.Time(0))
+        (trans3,rot3) = listener.lookupTransform("/ur5_arm_base_link", "/world",rospy.Time(0))
         # Publish the fake force
         fake_wrench = geometry_msgs.msg.WrenchStamped()
-        fake_wrench.wrench.force.x = marker_pose.translation.x - trans[0]
-        fake_wrench.wrench.force.y = marker_pose.translation.y - trans[1]
-        fake_wrench.wrench.force.z = marker_pose.translation.z - trans[2]
+        trans1_mat = tf.transformations.translation_matrix(trans1)
+        trans2_mat = tf.transformations.translation_matrix(trans2)
+        rot3_mat   = tf.transformations.quaternion_matrix(rot3)
+        mat1 = numpy.dot(rot3_mat, trans1_mat-trans2_mat)
+        force_at_arm_base_link = tf.transformations.translation_from_matrix(mat1)
+        fake_wrench.wrench.force.x = force_at_arm_base_link[0]
+        fake_wrench.wrench.force.y = force_at_arm_base_link[1]
+        fake_wrench.wrench.force.z = force_at_arm_base_link[2]
         rot = (marker_pose.rotation.x, marker_pose.rotation.y,
                 marker_pose.rotation.z, marker_pose.rotation.w)
         euler = tf.transformations.euler_from_quaternion(rot)
         fake_wrench.wrench.torque.x = euler[0]
         fake_wrench.wrench.torque.y = euler[1]
         fake_wrench.wrench.torque.z = euler[2]
-        fake_wrench.header.frame_id = "world"
+        fake_wrench.header.frame_id = "ur5_arm_base_link"
         fake_wrench.header.stamp = rospy.Time(0)
         wrench_pub.publish(fake_wrench)
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
         print "Coudn't find transforms!"
 
+def transformCallback( msg ):
+    br = TransformBroadcaster()
+    br.sendTransform((-marker_pose.translation.x,
+                     -marker_pose.translation.y,
+                     -marker_pose.translation.z),
+                     (0,0,0,1),
+                     rospy.Time.now(),
+                     "world",
+                     "fake_force_pose")
 
 def makeBox( msg ):
     marker = Marker()
@@ -58,6 +75,9 @@ def makeBoxControl( msg ):
     msg.controls.append( control )
     return control
 
+
+
+
 def processFeedback(feedback, br):
     s = "Feedback from marker '" + feedback.marker_name
     s += "' / control '" + feedback.control_name + "'"
@@ -76,13 +96,6 @@ def processFeedback(feedback, br):
             feedback.menu_entry_id) + " clicked" + mp + ".")
     elif feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
         rospy.loginfo(s + ": pose changed")
-        br.sendTransform((feedback.pose.position.x,
-                         feedback.pose.position.y,
-                         feedback.pose.position.z),
-                         (0, 0, 0, 1),
-                         rospy.Time.now(),
-                         "world",
-                         "fake_force_pose")
         marker_pose.translation.x = feedback.pose.position.x
         marker_pose.translation.y = feedback.pose.position.y
         marker_pose.translation.z = feedback.pose.position.z
@@ -101,10 +114,12 @@ if __name__ == "__main__":
     rospy.init_node("fake_force_sensor")
     br = TransformBroadcaster()
     listener = tf.TransformListener()
-    wrench_pub = rospy.Publisher('/wrench', geometry_msgs.msg.WrenchStamped, queue_size=1)
+    wrench_pub = rospy.Publisher('/wrench', geometry_msgs.msg.WrenchStamped, queue_size=4)
 
-     # Publisher for the wrench
-    rospy.Timer(rospy.Duration(0.02), frameCallback)
+    # Publisher for the topic
+    rospy.Timer(rospy.Duration(0.02), publisherCallback)
+    # Publisher for the TF
+    rospy.Timer(rospy.Duration(0.02), transformCallback)
 
     server = InteractiveMarkerServer("fake_force_sensor")
     menu_handler = MenuHandler()
@@ -126,7 +141,6 @@ if __name__ == "__main__":
     int_marker.header.frame_id = "world"
     int_marker.pose.position = position
     int_marker.scale = 1
-
     int_marker.name = "fake_wrench"
     int_marker.description = "Sim wrench \n Force = vector from UR5 ee to marker \n Torque = angle from initial orientation)"
 
@@ -160,7 +174,7 @@ if __name__ == "__main__":
     control.orientation.x = 0
     control.orientation.y = 1
     control.orientation.z = 0
-    control.name = "rotate_z"
+    control.name = "rotate_y"
     control.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
     if fixed:
         control.orientation_mode = InteractiveMarkerControl.FIXED
@@ -171,7 +185,7 @@ if __name__ == "__main__":
     control.orientation.x = 0
     control.orientation.y = 1
     control.orientation.z = 0
-    control.name = "move_z"
+    control.name = "move_y"
     control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
     if fixed:
         control.orientation_mode = InteractiveMarkerControl.FIXED
@@ -182,7 +196,7 @@ if __name__ == "__main__":
     control.orientation.x = 0
     control.orientation.y = 0
     control.orientation.z = 1
-    control.name = "rotate_y"
+    control.name = "rotate_z"
     control.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
     if fixed:
         control.orientation_mode = InteractiveMarkerControl.FIXED
@@ -193,7 +207,7 @@ if __name__ == "__main__":
     control.orientation.x = 0
     control.orientation.y = 0
     control.orientation.z = 1
-    control.name = "move_y"
+    control.name = "move_z"
     control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
     if fixed:
         control.orientation_mode = InteractiveMarkerControl.FIXED
